dc4e908d0a7a5ac215f2821442432d1be8291c98
notcodesid <notcodesid@gmail.com>
2024-07-06 13:21:56 +0530

    Theme layout added

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
+import ThemeTogglebtn from "@/components/common/ThemeTogglebtn"
+
 export default function Home() {
   return (
+    <>
    <h1> Hello from the Social Network</h1>
+   <ThemeTogglebtn />
+    </>
+   
   );
 }
