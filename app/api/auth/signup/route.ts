186e04c0ec3865bf576695b9f0e3c480b62b24b4
notcodesid <notcodesid@gmail.com>
2024-07-08 13:41:53 +0530

    setup postgres db with adding the signup

--- app/api/auth/signup/route.ts
+++ app/api/auth/signup/route.ts
@@ -2,6 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { signupSchema } from "@/app/model/validation";
 import { z } from "zod";
 import bcrypt from "bcrypt";
+import { prisma } from "@/app/db/db.config";
 
 
 export async function POST(req: NextRequest) {
@@ -9,9 +10,35 @@ export async function POST(req: NextRequest) {
         const body = await req.json();
         const validatedData = signupSchema.parse(body);
 
+        // check this email already exist
+        const checkEmailExist = await prisma.user.findUnique({
+            where : {
+                email : validatedData.email
+            }
+        })
+
+        if(checkEmailExist) {
+            return  NextResponse.json({
+                status : 400,
+                message : {
+                    email : "This email has already exist"
+                }
+
+            })
+        }
+
         // Hash the Password
         const saltRounds = 10;
         const hashedPassword = await bcrypt.hash(validatedData.password , saltRounds)
+
+        // insert data
+        await prisma.user.create({
+            data : {
+                name : validatedData.name,
+                email : validatedData.email,
+                password : hashedPassword
+            }
+        })
 
          // Create a new object with the hashed password
          const userDataToStore = {
@@ -28,6 +55,7 @@ export async function POST(req: NextRequest) {
         if (error instanceof z.ZodError) {
             return NextResponse.json({ message: 'Validation failed', errors: error.errors }, { status: 400 });
         } else {
+            console.log(error)
             return NextResponse.json({ message: 'An unexpected error occurred' }, { status: 500 });
         }
     }
